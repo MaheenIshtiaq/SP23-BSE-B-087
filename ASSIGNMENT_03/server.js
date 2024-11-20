58a074238a81041ae07bebe95039e88c888037bb
MAHEEN ISHTIAQ <153347167+MaheenIshtiaq@users.noreply.github.com>
2024-11-20 10:58:31 +0500

    ASSIGNMENT_03

--- ASSIGNMENT_03/server.js
+++ ASSIGNMENT_03/server.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
 
+// Serve static files (CSS, JS, etc.) from the public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Route to serve tesco_005.html at the root '/'
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    res.sendFile(path.join(__dirname,'views', 'tesco_005.html'));
 });
 
-app.get('/bootstrap', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'tesco_005.html'));
+// Route to serve portfolio page at '/portfolio'
+app.get('/portfolio', (req, res) => {
+    res.sendFile(path.join(__dirname,'views', 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+// Start server on port 3000
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
